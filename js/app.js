47ad38d80c303fa8ca15986725568041724335f2
epson121 <traxdater12@gmail.com>
2013-10-09 16:35:35 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
 App = Ember.Application.create();
 
-App.Router.map(function(){
-	this.resource("markdown");
-});
-
-App.MarkdownRoute = Ember.Route.extend({
-  model: function() {
-  }
-})
-
 var showdown = new Showdown.converter();
 
 Ember.Handlebars.helper('format-markdown', function(input) {
